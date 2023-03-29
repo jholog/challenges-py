b1a1cc3fa0c4b678a92b5af101017fabed847c5a
jholog <wnsgh0304@gmail.com>
2023-03-29 14:39:38 +0900

    Modify beakjoon loop (11 to 12)

--- baekjoon/step/step3/loop_11.py
+++ baekjoon/step/step3/loop_11.py
@@ -1,7 +1,11 @@
 # https://www.acmicpc.net/problem/10952
 
-while True:
-    a, b = map(int, input().split())
-    if a == 0 or b == 0:
-        break
-    print(a + b)
+# while True:
+#     a, b = map(int, input().split())
+#     if a == 0 or b == 0:
+#         break
+#     print(a + b)
+
+# walrus operator(:=) in Python 3.8
+while a := eval("+".join(input().split())):
+    print(a)
--- baekjoon/step/step3/loop_12.py
+++ baekjoon/step/step3/loop_12.py
@@ -4,5 +4,7 @@ while True:
     try:
         a, b = map(int, input().split())
         print(a + b)
-    except:
+    except:     # EOFError or ValueError
         break
+
+# lines = sys.stdin.readlines() >> 대량인 경우 속도 면에서 input()보다 유리
